18fee68b0d723f9db2887388a1d649d5f675e41b
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2023-11-11 21:09:01 +0100

    Refine `FutureAtom`

--- lib/async_state.dart
+++ lib/async_state.dart
@@ -1,23 +1,10 @@
 import 'dart:math';
 
-import 'package:declarative_reactivity_workshop/src/async_value.dart';
+import 'src/async_atom.dart';
 
-import 'src/atom.dart';
-
-final delayed = Atom<AsyncValue<int>>((context) {
-  final value = context.mutateSelf((value) {
-    if (value?.valueOrNull case final value?) {
-      return AsyncLoading(value);
-    }
-
-    return value;
-  });
-
-  Future.delayed(const Duration(seconds: 2), () {
-    context.mutateSelf(
-      (_) => AsyncData(Random().nextInt(100)),
-    );
-  });
-
-  return value ?? const AsyncLoading();
+final delayed = FutureAtom((_) {
+  return Future.delayed(
+    const Duration(seconds: 2),
+    () => Random().nextInt(100),
+  );
 });
