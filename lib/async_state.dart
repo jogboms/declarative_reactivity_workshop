c786c38527c290441588acb311e65cd6eb8f1633
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2023-12-09 23:21:09 +0100

    Include debug names to state

--- lib/async_state.dart
+++ lib/async_state.dart
@@ -8,11 +8,11 @@ final delayed = FutureAtom((_) {
     const Duration(seconds: 2),
     () => Random().nextInt(100),
   );
-});
+}, name: 'delayed');
 
 final delayedByTen = FutureAtom((context) async {
   return await context.async(delayed) * 10;
-});
+}, name: 'delayed-by-ten');
 
 final delayedStream = StreamAtom((context) async* {
   final value = await context.async(delayed);
@@ -20,22 +20,22 @@ final delayedStream = StreamAtom((context) async* {
   await Future.delayed(const Duration(seconds: 2));
 
   yield value;
-});
+}, name: 'delayed-stream');
 
 final delayedStreamByTen = FutureAtom((context) async {
   return await context.async(delayedStream) * 10;
-});
+}, name: 'delayed-stream-by-ten');
 
 final delayedStreamCounter = StreamAtom((context) async* {
   yield* _countGenerator().asyncMap(
     (count) async => (await context.async(delayedStream)) + count,
   );
-});
+}, name: 'delayed-stream-counter');
 
 final delayedByStream = FutureAtom((context) async {
   await Future.delayed(const Duration(milliseconds: 300));
   return await context.async(delayedStreamCounter);
-});
+}, name: 'delayed-by-stream');
 
 Stream<int> _countGenerator() {
   return Stream.periodic(
--- lib/state.dart
+++ lib/state.dart
@@ -3,14 +3,17 @@ import 'dart:async';
 import 'log.dart';
 import 'src/atom.dart';
 
-final firstname = Atom((_) => 'First');
-final lastname = Atom((_) => 'Last');
-final age = Atom((_) => 0);
+final firstname = Atom((_) => 'First', name: 'firstname');
+final lastname = Atom((_) => 'Last', name: 'lastname');
+final age = Atom((_) => 0, name: 'age');
 
-final doubleAge = Atom((context) => context.get(age) * 2.0);
-final result = Atom((context) => '${context.get(firstname)} ${context.get(lastname)} (${context.get(age)})');
+final doubleAge = Atom((context) => context.get(age) * 2.0, name: 'double-age');
+final result = Atom(
+  (context) => '${context.get(firstname)} ${context.get(lastname)} (${context.get(age)})',
+  name: 'result',
+);
 
-final passThrough = Atom.family((_, int value) => value);
+final passThrough = Atom.family((_, int value) => value, name: 'pass-through');
 
 final personState = Atom(
   (context) => (
@@ -18,6 +21,7 @@ final personState = Atom(
     fullname: '${context.get(firstname)} ${context.get(lastname)}',
     canDrive: context.get(age) > 4,
   ),
+  name: 'person-state',
 );
 
 final counter = Atom((AtomContext<int> context) {
@@ -34,13 +38,13 @@ final counter = Atom((AtomContext<int> context) {
   });
 
   return 0;
-});
+}, name: 'counter');
 
-final shouldTrackAge = Atom((_) => true);
+final shouldTrackAge = Atom((_) => true, name: 'should-track-age');
 final maybeTrackAge = Atom((context) {
   log('rebuild', 'maybe-track-age');
   if (context.get(shouldTrackAge)) {
     return context.get(age);
   }
   return -1;
-});
+}, name: 'maybe-track-age');
